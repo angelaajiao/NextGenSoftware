c0c47551609070a7ed7331e9e4b8a130bb7829be
angelaajiao <121437129+angelaajiao@users.noreply.github.com>
2024-10-09 10:40:27 +0200

    Clase cliente con get y set

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/Cliente.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/Cliente.java
@@ -9,4 +9,61 @@ public class Cliente {
     private String nombre;
     private String apellidos;
     private String dni;
+
+    //Constructor
+    public Cliente(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+
+    //Métodos get y set
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Collection<Restaurante> getFavoritos() {
+        return favoritos;
+    }
+
+    public void setFavoritos(Collection<Restaurante> favoritos) {
+        this.favoritos = favoritos;
+    }
+
+    public Collection<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Collection<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public Collection<Direccion> getDirecciones() {
+        return direcciones;
+    }
+
+    public void setDirecciones(Collection<Direccion> direcciones) {
+        this.direcciones = direcciones;
+    }
+
 }
