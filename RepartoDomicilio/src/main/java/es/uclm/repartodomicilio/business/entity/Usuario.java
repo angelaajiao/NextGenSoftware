4e31c62675aed618e88ba350df67887cd78eb6f7
angelaajiao <121437129+angelaajiao@users.noreply.github.com>
2024-10-22 10:27:02 +0200

    Clase usuario para bbdd

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/Usuario.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/Usuario.java
@@ -1,17 +1,43 @@
-/*package es.uclm.repartodomicilio.business.entity;
+package es.uclm.repartodomicilio.business.entity;
 
 import jakarta.persistence.*;
-
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String idUsuario;
+    @Column
     private String pass;
-    private int attribute;
+    @Column
+    private int attribute; //restaurante, cliente, repartidor
 
+    public Usuario(){}
     // Creamos el constructor
-    public Usuario(String idUsuario, String pass, int attribute) {
+    public Usuario(String pass, int attribute) {
         super();
         this.pass = pass;
         this.attribute = attribute;
     }
+
+    // Métodos get y set
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Usuario [idUsuario=%s, pass=%s, attribute=%s]", idUsuario, pass, attribute);
+    }
 }
-*/
