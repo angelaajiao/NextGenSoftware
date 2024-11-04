30e06bad37277ffd3a55f620173a490ed3843891
angelaajiao <121437129+angelaajiao@users.noreply.github.com>
2024-11-04 23:52:38 +0100

    cambio nombre

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/controller/GestorRepartos.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/controller/GestorRepartos.java
@@ -1,15 +1,12 @@
 package es.uclm.repartodomicilio.business.controller;
 
 import  es.uclm.repartodomicilio.business.entity.Repartidor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import  org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import  org.springframework.beans.factory.annotation.Autowired;
 import es.uclm.repartodomicilio.business.persistence.RepartidorDAO;
 
@@ -32,7 +29,7 @@ public class GestorRepartos {
         // Guardamos el repartidor en la base de datos
         repartidorDAO.save(repartidor);
         model.addAttribute("repartidor", repartidor);
-        return "result";
+        return "resultRepartidor";
     }
 
 
--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/controller/GreetingController.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/controller/GreetingController.java
@@ -34,7 +34,7 @@ public class GreetingController {
         model.addAttribute("greeting", greeting);
         Greeting savedGreeting = greetingDAO.save(greeting);
         log.info("Saved greeting: " + savedGreeting);
-        return "result";
+        return "greeting";
     }
 
 }
