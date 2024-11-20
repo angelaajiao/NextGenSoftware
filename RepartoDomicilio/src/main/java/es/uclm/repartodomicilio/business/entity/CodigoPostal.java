91385916c6b52e41804c68c633df7cc6b3af7439
angelaajiao <121437129+angelaajiao@users.noreply.github.com>
2024-11-20 12:04:33 +0100

    enumeracion de codigo postal

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/CodigoPostal.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/CodigoPostal.java
@@ -1,6 +1,19 @@
 package es.uclm.repartodomicilio.business.entity;
 
-public enum CodigoPostal{
-    	45600,
-        28000
+public enum CodigoPostal {
+    TALAVERA(45600),
+    MADRID(28000);
+
+    private final int codigo;
+
+    // Constructor
+    CodigoPostal(int codigo) {
+        this.codigo = codigo;
+    }
+
+    // Método para obtener el valor numérico
+    public int getCodigo() {
+        return codigo;
+    }
 }
+
