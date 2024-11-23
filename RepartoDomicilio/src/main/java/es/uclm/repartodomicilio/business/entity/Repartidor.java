46158342028dbf51a6c59f9e5b6dd9ba89785cc2
Huda <Houda.Elouahabi@alu.uclm.es>
2024-11-23 01:29:14 +0100

    implementadas clases carta menu

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/Repartidor.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/Repartidor.java
@@ -12,8 +12,8 @@ public class Repartidor {
     @Column(name = "dni_repartidor", nullable = false)
     private String dniRepartidor;
 
-    @Column(nullable = false)
-    private String nombreRepartidor;
+    //@Column(nullable = false)
+    //private String nombreRepartidor;
 
     @Column(nullable = false)
     private String apellidoRepartidor;
@@ -37,7 +37,7 @@ public class Repartidor {
     // Constructor
     public Repartidor(String dniRepartidor, String nombreRepartidor, String apellidoRepartidor, String passwordRepartidor, String emailRepartidor, boolean disponible) {
         this.dniRepartidor = dniRepartidor;
-        this.nombreRepartidor = nombreRepartidor;
+        //this.nombreRepartidor = nombreRepartidor;
         this.apellidoRepartidor = apellidoRepartidor;
         this.ContrasenaRepartidor = passwordRepartidor;
         this.emailRepartidor = emailRepartidor;
@@ -61,13 +61,13 @@ public class Repartidor {
         this.dniRepartidor = dniRepartidor;
     }
 
-    public String getNombreRepartidor() {
+    /*public String getNombreRepartidor() {
         return nombreRepartidor;
-    }
+    }*/
 
-    public void setNombreRepartidor(String nombreRepartidor) {
+   /* public void setNombreRepartidor(String nombreRepartidor) {
         this.nombreRepartidor = nombreRepartidor;
-    }
+    }*/
 
     public String getApellidoRepartidor() {
         return apellidoRepartidor;
