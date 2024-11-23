2e1c922cfbb69241b51f03935042c6e628b096f9
Huda <Houda.Elouahabi@alu.uclm.es>
2024-11-23 01:30:44 +0100

    implementadas clases carta menu

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/TipoItemMenu.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/entity/TipoItemMenu.java
@@ -1,7 +1,42 @@
 package es.uclm.repartodomicilio.business.entity;
+//Entrada, Principal, Postre
 
-public enum TipoItemMenu {
-    COMIDA,
-    BEBIDA,
-    POSTRE
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class TipoItemMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "tipo", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemMenu> items;
+
+    // Getters, Setters y Constructor
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<ItemMenu> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemMenu> items) {
+        this.items = items;
+    }
 }
