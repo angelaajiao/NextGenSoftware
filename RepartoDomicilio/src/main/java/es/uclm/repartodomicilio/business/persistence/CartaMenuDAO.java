389639629c6671d75a904bc23a69283eacca798e
Huda <Houda.Elouahabi@alu.uclm.es>
2024-11-28 02:11:23 +0100

    carta menu sin iniciar sesion funcional

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/persistence/CartaMenuDAO.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/persistence/CartaMenuDAO.java
@@ -4,8 +4,11 @@ import es.uclm.repartodomicilio.business.entity.CartaMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartaMenuDAO extends JpaRepository<CartaMenu, Long> {
-
 }
+
 
