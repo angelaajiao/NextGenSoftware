838adcb35ffae5947189fe8853d340c7e6522108
sara.gutierrez <94965519+sgcaja@users.noreply.github.com>
2024-12-04 11:03:57 +0100

    Correción de error endpoint vista cliente, logout

--- RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/controller/GestorClientes.java
+++ RepartoDomicilio/src/main/java/es/uclm/repartodomicilio/business/controller/GestorClientes.java
@@ -4,12 +4,12 @@ package es.uclm.repartodomicilio.business.controller;
 import es.uclm.repartodomicilio.business.entity.Cliente;
 import es.uclm.repartodomicilio.business.entity.Restaurante;
 import es.uclm.repartodomicilio.business.persistence.ClienteDAO;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import es.uclm.repartodomicilio.business.persistence.RestauranteDAO;
-
 
 import java.util.List;
 
@@ -19,6 +19,8 @@ public class GestorClientes {
     @Autowired
     private ClienteDAO clienteDAO; // Asegúrate de que este DAO esté bien definido
 
+    @Autowired
+    private HttpSession session;
     @Autowired
     private RestauranteDAO restauranteDAO;
 
@@ -69,6 +71,7 @@ public class GestorClientes {
 
     @GetMapping("/Cliente")
     public String mostrarCliente(Model model) {
+
         List<Restaurante> restaurantes = restauranteDAO.findAll(); // Obtener todos los restaurantes
         model.addAttribute("restaurantes", restaurantes);
         return "VistaCliente";
@@ -76,10 +79,12 @@ public class GestorClientes {
 
     @PostMapping("/Cliente")
     public String listarRestaurantes(Model model) {
-        List<Restaurante> restaurantes = restauranteDAO.findAll(); // Obtener todos los restaurantes
+        List<Restaurante> restaurantes = restauranteDAO.findAll();
         model.addAttribute("restaurantes", restaurantes);
         return "VistaCliente"; // Nombre de la vista donde se mostrará la lista
     }
+
+
 
 }
 
